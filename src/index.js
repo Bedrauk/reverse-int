96fc01c720106064d3b98e176b1a1c1621aeb314
Bedrauk <111381113+Bedrauk@users.noreply.github.com>
2024-07-31 19:46:10 +0300

    docs: code again

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    function reverse (n) {
+        let tmp = Math.abs(n), res = '';
+        while (tmp) {
+            res += String(tmp % 10);
+            res = (res.startsWith('0') ? res.slice(1,-1) : res)
+            tmp = Math.floor(tmp / 10)
+        }
+    
+        return res
+    }
+    
 }
